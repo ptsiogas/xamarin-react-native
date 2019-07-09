57c545dc9b9e9541ddf57de577cfad0ddd9253cb
Paris Tsiogas <tsiogas@thinkdesquared.com>
2019-07-09 18:45:01 +0300

    FIX - asking for OVERLAY_PERMISSION

--- samples/SampleApp.Droid/MainActivity.cs
+++ samples/SampleApp.Droid/MainActivity.cs
@@ -12,7 +12,7 @@ using Android.Provider;
 
 namespace SampleApp.Droid
 {
-    [Activity(Label = "XamarinReactNative", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    [Activity (Label = "XamarinReactNative", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
     public class MainActivity : AppCompatActivity, IDefaultHardwareBackBtnHandler
     {
         ReactRootView mReactRootView;
@@ -21,108 +21,133 @@ namespace SampleApp.Droid
 
         private const int OVERLAY_PERMISSION_REQ_CODE = 1;
 
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate (Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate (savedInstanceState);
 
             if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
             {
-                if (!Settings.CanDrawOverlays(this))
+                if (!Settings.CanDrawOverlays (this))
                 {
-                    Intent intent = new Intent(Settings.ActionManageOverlayPermission, Uri.Parse("package:" + PackageName));
-                    StartActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+                    Intent intent = new Intent (Settings.ActionManageOverlayPermission, Uri.Parse ("package:" + PackageName));
+                    StartActivityForResult (intent, OVERLAY_PERMISSION_REQ_CODE);
+                }
+                else
+                {
+                    startReactNative ();
                 }
             }
+            else
+            {
+                startReactNative ();
+            }
 
-            mReactRootView = new ReactRootView(this);
-            mReactInstanceManager = ReactInstanceManager.Builder()
-                    .SetApplication(Application)
-                    .SetBundleAssetName("index.android.bundle")
-                    .SetJSMainModulePath("index")
-                    .AddPackage(new MainReactPackage())
+
+        }
+
+        private void startReactNative ()
+        {
+            mReactRootView = new ReactRootView (this);
+            mReactInstanceManager = ReactInstanceManager.Builder ()
+                    .SetApplication (Application)
+                    .SetBundleAssetName ("index.android.bundle")
+                    .SetJSMainModulePath ("index")
+                    .AddPackage (new MainReactPackage ())
 #if DEBUG
-                    .SetUseDeveloperSupport(true)
+                    .SetUseDeveloperSupport (true)
 #else
                     .SetUseDeveloperSupport(false)
 #endif
-                    .SetInitialLifecycleState(LifecycleState.Resumed)
-                    .Build();
+                    .SetInitialLifecycleState (LifecycleState.Resumed)
+                    .Build ();
 
-            mReactRootView.StartReactApplication(mReactInstanceManager, "MyReactNativeApp", null);
+            mReactRootView.StartReactApplication (mReactInstanceManager, "MyReactNativeApp", null);
 
-            SetContentView(mReactRootView);
+            SetContentView (mReactRootView);
         }
 
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        protected override void OnActivityResult (int requestCode, Result resultCode, Intent data)
         {
             if (requestCode == OVERLAY_PERMISSION_REQ_CODE)
             {
                 if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
                 {
-                    if (!Settings.CanDrawOverlays(this))
+                    if (!Settings.CanDrawOverlays (this))
                     {
                         // SYSTEM_ALERT_WINDOW permission not granted...
+                        try
+                        {
+                            startReactNative ();
+                        }
+                        catch(System.Exception e)
+                        {
+                            //workaround
+                        }
+                    }
+                    else
+                    {
+                        startReactNative ();
                     }
                 }
             }
         }
 
-        protected override void OnPause()
+        protected override void OnPause ()
         {
-            base.OnPause();
+            base.OnPause ();
 
             if (mReactInstanceManager != null)
             {
-                mReactInstanceManager.OnHostPause(this);
+                mReactInstanceManager.OnHostPause (this);
             }
         }
 
-        protected override void OnResume()
+        protected override void OnResume ()
         {
-            base.OnResume();
+            base.OnResume ();
 
             if (mReactInstanceManager != null)
             {
-                mReactInstanceManager.OnHostResume(this, this);
+                mReactInstanceManager.OnHostResume (this, this);
             }
         }
 
-        protected override void OnDestroy()
+        protected override void OnDestroy ()
         {
-            base.OnDestroy();
+            base.OnDestroy ();
 
             if (mReactInstanceManager != null)
             {
-                mReactInstanceManager.OnHostDestroy(this);
+                mReactInstanceManager.OnHostDestroy (this);
             }
         }
 
-        public override void OnBackPressed()
+        public override void OnBackPressed ()
         {
             if (mReactInstanceManager != null)
             {
-                mReactInstanceManager.OnBackPressed();
+                mReactInstanceManager.OnBackPressed ();
             }
             else
             {
-                base.OnBackPressed();
+                base.OnBackPressed ();
             }
         }
 
-        public void InvokeDefaultOnBackPressed()
+        public void InvokeDefaultOnBackPressed ()
         {
-            base.OnBackPressed();
+            base.OnBackPressed ();
         }
 
-        public override bool OnKeyUp(Keycode keyCode, KeyEvent e)
+        public override bool OnKeyUp (Keycode keyCode, KeyEvent e)
         {
             if (keyCode == Keycode.Menu && mReactInstanceManager != null)
             {
-                mReactInstanceManager.ShowDevOptionsDialog();
+                mReactInstanceManager.ShowDevOptionsDialog ();
                 return true;
             }
 
-            return base.OnKeyUp(keyCode, e);
+            return base.OnKeyUp (keyCode, e);
         }
     }
 }
